5ec228370540e107b6647ccf7b0134af3fe1926f
박상언 <91855448+Blueslime0216@users.noreply.github.com>
2024-12-20 15:42:26 +0900

    003-3
    하교 push

--- src/sys/keyboard.ts
+++ src/sys/keyboard.ts
@@ -7,36 +7,32 @@ import controller from "./controller"; // 컨트롤러 클래스를 불러옴
 
 export class Keyboard {
     // 현재 눌려있는 키를 저장하는 Set 객체 (중복된 값을 허용하지 않음)
-    private _keymap:Array<KeyboardEvent["code"]> = [];      // 눌린 키
-    private _keymap_hold:Array<KeyboardEvent["code"]> = []; // 꾹 눌리고 있는 키
-    private _time:Array<number> = [];                       // 눌린 시간
-
-    get keymap() { return this._keymap; }
-    get keymap_hold() { return this._keymap_hold; }
-    get time() { return this._time; }
+    keymap:Array<KeyboardEvent["code"]> = [];      // 눌린 키
+    keymap_hold:Array<KeyboardEvent["code"]> = []; // 꾹 눌리고 있는 키
+    time:Array<number> = [];                       // 눌린 시간
 
     // 특정 key가 눌려있는지 확인
-    isKeyDown(key:KeyboardEvent["code"]): boolean { return this._keymap.indexOf(key) !== -1; }
-    isKeyHold(key:KeyboardEvent["code"]): boolean { return this._keymap_hold.indexOf(key) !== -1; }
+    isKeyDown(key:KeyboardEvent["code"]): boolean { return $keyboard.keymap.indexOf(key) !== -1; }
+    isKeyHold(key:KeyboardEvent["code"]): boolean { return $keyboard.keymap_hold.indexOf(key) !== -1; }
     // index
-    indexOf(key:KeyboardEvent["code"]): number { return this._keymap.indexOf(key); }
+    indexOf(key:KeyboardEvent["code"]): number { return $keyboard.keymap.indexOf(key); }
 
 
     // 키보드 이벤트 리스너
     keydown(e: KeyboardEvent) {
         // 키 감지하는 부분
-        if (this._keymap.indexOf(e.code) === -1) { // 눌린 키가 _keymap에 없다면 keydown 판정
+        if ($keyboard.keymap.indexOf(e.code) === -1) { // 눌린 키가 keymap에 없다면 keydown 판정
             // keymap에 해당 키 추가
-            this._keymap.push(e.code);
+            $keyboard.keymap.push(e.code);
             // 시간도 추가
-            this._time.push(e.timeStamp);
+            $keyboard.time.push(e.timeStamp);
 
             // keydown 실행
             controller.run('keydown', e);
         } else { // 중복된 값이 있으면
-            if (this._keymap_hold.indexOf(e.code) === -1) { // _keymap_hold에 중복된 값이 없으면
+            if ($keyboard.keymap_hold.indexOf(e.code) === -1) { // keymap_hold에 중복된 값이 없으면
                 // holdStart 판정
-                this._keymap_hold.push(e.code);
+                $keyboard.keymap_hold.push(e.code);
 
                 controller.run('holdStart', e);
             } else {
@@ -47,13 +43,13 @@ export class Keyboard {
     }
     keyup(e: KeyboardEvent) {
         // keymap에서 키 제거
-        const index = this._keymap.indexOf(e.code);
-        const index_hold = this._keymap_hold.indexOf(e.code);
-        this._keymap.splice(index, 1);
-        this._keymap_hold.splice(index_hold, 1);
+        const index = $keyboard.keymap.indexOf(e.code);
+        const index_hold = $keyboard.keymap_hold.indexOf(e.code);
+        $keyboard.keymap.splice(index, 1);
+        $keyboard.keymap_hold.splice(index_hold, 1);
 
         // 시간도 제거
-        this._time.splice(index, 1);
+        $keyboard.time.splice(index, 1);
 
         controller.run('keyup', e);
     }
